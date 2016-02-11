e2f5446dd78f13466556da0e5f006ac345d6e24f
Kelly Dwan <kelly.dwan@gmail.com>
2016-02-10 20:06:23 -0500

    Add sass & watch

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,12 +1,28 @@
 var gulp = require( 'gulp' );
+var sass = require('gulp-sass');
 var postcss = require( 'gulp-postcss' );
 var autoprefixer = require( 'autoprefixer' );
+var watch = require('gulp-watch');
 
 gulp.task( 'css', function () {
-    var processors = [
-        autoprefixer( { browsers: ['last 1 version'] } )
-    ];
-    return gulp.src( './style.scss' )
-        .pipe( postcss( processors ) )
-        .pipe( gulp.dest( './' ) );
+	var processors = [
+		autoprefixer( { browsers: ['last 2 versions'] } )
+	];
+	return gulp.src( './style.scss' )
+		.pipe( sass().on( 'error', sass.logError ) )
+		.pipe( postcss( processors ) )
+		.pipe( gulp.dest( './' ) );
+} );
+
+gulp.task( 'default', function (cb) {
+	watch( './style.scss', function() {
+		var processors = [
+			autoprefixer( { browsers: ['last 2 versions'] } )
+		];
+		gulp.src( './style.scss' )
+			.pipe( sass().on( 'error', sass.logError ) )
+			.pipe( postcss( processors ) )
+			.pipe( gulp.dest( './' ) )
+			.on( 'end', cb );
+	} );
 } );
