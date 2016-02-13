a4ec9cf20825a602baae660f3ab57e0470f8d766
Kelly Dwan <kelly.dwan@gmail.com>
2016-02-13 12:32:51 -0500

    Move files into `sass` folder

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,27 +2,19 @@ var gulp = require( 'gulp' );
 var sass = require('gulp-sass');
 var postcss = require( 'gulp-postcss' );
 var autoprefixer = require( 'autoprefixer' );
-var watch = require('gulp-watch');
 
 gulp.task( 'css', function () {
 	var processors = [
 		autoprefixer( { browsers: ['last 2 versions'] } )
 	];
-	return gulp.src( './style.scss' )
+	return gulp.src( './sass/style.scss' )
 		.pipe( sass().on( 'error', sass.logError ) )
 		.pipe( postcss( processors ) )
-		.pipe( gulp.dest( './' ) );
+		.pipe( gulp.dest( '.' ) );
 } );
 
-gulp.task( 'default', function (cb) {
-	watch( './style.scss', function() {
-		var processors = [
-			autoprefixer( { browsers: ['last 2 versions'] } )
-		];
-		gulp.src( './style.scss' )
-			.pipe( sass().on( 'error', sass.logError ) )
-			.pipe( postcss( processors ) )
-			.pipe( gulp.dest( './' ) )
-			.on( 'end', cb );
-	} );
+gulp.task( 'watch', function() {
+	gulp.watch( 'sass/**/*.scss', [ 'css' ] );
 } );
+
+gulp.task( 'default', [ 'watch', 'css' ] );
