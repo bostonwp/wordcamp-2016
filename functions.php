12bda63ddf6c50ff82da9c4b578ba2dcd67b576d
Kelly Dwan <kelly.dwan@gmail.com>
2016-07-18 19:04:14 -0400

    Add this site to the “new feature” whitelists.
    As features are added to WordCamp.org, they’re gated behind min-site-ID conditions. I want to use these on my local site, so it needs to be whitelisted.

--- functions.php
+++ functions.php
@@ -8,3 +8,13 @@ function boston_2016_styles() {
 	wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 }
 add_action( 'wp_enqueue_scripts', 'boston_2016_styles' );
+
+function boston_2016_enable_features( $sites ) {
+	$sites[] = 47;
+	return $sites;
+}
+add_filter( 'wcpt_speaker_post_avatar_enabled_site_ids',       'boston_2016_enable_features' );
+add_filter( 'wcpt_session_post_speaker_info_enabled_site_ids', 'boston_2016_enable_features' );
+add_filter( 'wcpt_session_post_slides_info_enabled_site_ids',  'boston_2016_enable_features' );
+add_filter( 'wcpt_session_post_video_info_enabled_site_ids',   'boston_2016_enable_features' );
+add_filter( 'wcpt_speaker_post_session_info_enabled_site_ids', 'boston_2016_enable_features' );
